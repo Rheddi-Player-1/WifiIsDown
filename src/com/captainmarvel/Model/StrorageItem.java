3077f4e33ed9d777245821685b2d0d15d32e69a7
AnneJ17 <ajoseph13@ggc.edu>
2019-11-17 19:59:33 -0500

    StrorageItem updated

--- src/com/captainmarvel/Model/StrorageItem.java
+++ src/com/captainmarvel/Model/StrorageItem.java
@@ -2,5 +2,22 @@ package com.captainmarvel.Model;
 
 public class StrorageItem extends Item
 {
+    private int itemCapacity;
+
+    public StrorageItem(String itemID, String itemName, String itemDescription, int itemCapacity)
+    {
+        super(itemID, itemName, itemDescription);
+        this.itemCapacity = itemCapacity;
+    }
+
+    public int getItemSize()
+    {
+        return itemCapacity;
+    }
+
+    public void setItemSize(int itemCapacity)
+    {
+        this.itemCapacity = itemCapacity;
+    }
 
 }
