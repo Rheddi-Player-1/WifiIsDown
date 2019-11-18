7b65943014e7aa67862066fae384a7ba7c16e4d9
AnneJ17 <ajoseph13@ggc.edu>
2019-11-17 19:54:17 -0500

    updated KeyItem

--- src/com/captainmarvel/Model/KeyItem.java
+++ src/com/captainmarvel/Model/KeyItem.java
@@ -1,6 +1,22 @@
-package final_prj;
+package com.captainmarvel.Model;
 
-public class KeyItem extends Item
-{
-	
-}
+    public class KeyItem extends Item
+    {
+        private int itemSize;
+
+        public KeyItem(String itemID, String itemName, String itemDescription, int itemSize)
+        {
+            super(itemID, itemName, itemDescription);
+            this.itemSize = itemSize;
+        }
+
+        public int getItemSize()
+        {
+            return itemSize;
+        }
+
+        public void setItemSize(int itemSize)
+        {
+            this.itemSize = itemSize;
+        }
+    }
